50660f665dcd78e154ce84b6d29f4484955aa1d8
Sara-Monica <sfsmc6@gmail.com>
2015-08-21 08:43:58 -0700

    Pokemon

---
+++ Pokemon/Pokemon.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Pokemon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pokemon
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Pikachu welcomes you to the world of Pokemon!");
+        System.out.println("\t (\\__/)");
+        System.out.println("\t (o^.^)");
+        System.out.println("\tz(_\")(\")");
+    }
+}
+        
