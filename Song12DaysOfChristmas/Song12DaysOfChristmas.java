fe8e2393939b55460dcd33d568602e0679442b34
Sara-Monica <sfsmc6@gmail.com>
2015-08-27 17:59:14 -0700

    Song12DaysOfChristmas

---
+++ Song12DaysOfChristmas/Song12DaysOfChristmas.java
@@ -0,0 +1,112 @@
+
+/**
+ * Write a description of class Song12DaysOfChristmas here.
+ * 
+ * @Sara Cisneros
+ * @August 27,2015
+ */
+public class Song12DaysOfChristmas
+{
+    public static void main(String[] args)
+    {
+        System.out.print("On the first ");
+        truelove ();
+        partridge ();
+        System.out.print("On the second ");
+        truelove ();
+        turtledove ();
+        System.out.print("On the third ");
+        truelove ();
+        frenchhen ();
+        System.out.print("On the fourth ");
+        truelove ();
+        callingbird ();
+        System.out.print("On the fifth ");
+        truelove ();
+        goldring ();
+        System.out.print("On the sixth ");
+        truelove ();
+        geeselaying ();
+        System.out.print("On the seventh ");
+        truelove ();
+        swanswimming ();
+        System.out.print("On the eighth ");
+        truelove ();
+        maidmilking ();
+        System.out.print("On the nineth ");
+        truelove ();
+        ladiesdancing ();
+        System.out.print("On the tenth ");
+        truelove ();
+        lordleaping ();
+        System.out.print("On the tenth ");
+        truelove ();
+        piperspiping ();
+        System.out.print("On the twelfth ");
+        truelove ();
+        drummersdrumming ();
+    }
+    public static void truelove ()
+    {
+        System.out.println("day of Christmas my true love sent to me");
+    }
+    public static void partridge ()
+    {
+        System.out.println("A Partridge in a Pear Tree");
+    }
+    public static void turtledove ()
+    {
+        System.out.println("Two Turtle Doves and");
+        partridge ();
+    }
+    public static void frenchhen ()
+    {
+        System.out.println("Three French Hens");
+        turtledove ();
+    }
+    public static void callingbird ()
+    {
+          System.out.println("Four Calling Birds.");
+          frenchhen ();
+    }
+    public static void goldring ()
+    {
+        System.out.println("Five Golden Rings,");
+        callingbird ();
+    }
+    public static void geeselaying ()
+    {
+        System.out.println("Six Geese a laying.");
+        goldring ();
+    }
+    public static void swanswimming ()
+    {
+        System.out.println("Seven Swans a Swimming");
+        geeselaying ();
+    }
+    public static void maidmilking ()
+    {
+        System.out.println("Eight Maids a Milking");
+        swanswimming ();
+    }
+    public static void ladiesdancing ()
+    {
+        System.out.println("Nine Ladies Dancing");
+        maidmilking ();
+    }
+    public static void lordleaping ()
+    {
+        System.out.println("Ten Lords a Leaping");
+        ladiesdancing ();
+    }
+    public static void piperspiping ()
+    {
+        System.out.println("Eleven Pipers Piping.");
+        lordleaping ();
+    }
+    public static void drummersdrumming ()
+    {
+        System.out.println("Twelve Drummers Drumming");
+        lordleaping ();
+    }
+}
