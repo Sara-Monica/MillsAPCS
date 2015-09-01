c6bce01c9dd8e1747ab6b7b4c6e083ebf3d8adc9
Sara-Monica <sfsmc6@gmail.com>
2015-08-31 17:18:52 -0700

    Mississippi

--- Mississippi/Mississippi.java
+++ Mississippi/Mississippi.java
@@ -3,105 +3,105 @@ public class Mississippi
 {
   public static void main(String[] args)
   {
-      letterm ();
-      letteri ();
-      letters ();
-      letters ();
-      letteri ();
-      letters ();
-      letters ();
-      letteri ();
-      letterp ();
-      letterp ();
-      letteri ();
+      Letterm ();
+      Letteri ();
+      Letters ();
+      Letters ();
+      Letteri ();
+      Letters ();
+      Letters ();
+      Letteri ();
+      Letterp ();
+      Letterp ();
+      Letteri ();
     }
-    public static void letterm ()
+    public static void Letterm ()
     {
-        m2apart ();
-        m2close ();
-        m4letters ();
-        m3letters ();
-        m2apart ();
+        M2apart ();
+        M2close ();
+        M4letters ();
+        M3letters ();
+        M2apart ();
     }
-    public static void letteri ()
+    public static void Letteri ()
     {
-        i4letters ();
-        i1letters ();
-        i1letters ();
-        i1letters ();
-        i1letters ();
-        i1letters ();
-        i4letters ();
+        I4letters ();
+        I1letters ();
+        I1letters ();
+        I1letters ();
+        I1letters ();
+        I1letters ();
+        I4letters ();
     }
-    public static void letters ()
+    public static void Letters ()
     {
-        s4letters ();
-        s2letters ();
-        s1letters ();
-        s4letters ();
-        s1bletters ();
-        s2letters ();
-        s4letters ();
+        S4letters ();
+        S2letters ();
+        S1lletters ();
+        S4letters ();
+        S1rletters ();
+        S2letters ();
+        S4letters ();
     }
-    public static void letterp ()
+    public static void Letterp ()
     {
-        p6letters ();
-        p2letters ();
-        p2letters ();
-        p6letters ();
-        p1letters ();
-        p1letters ();
-        p1letters ();
+        P6letters ();
+        P2letters ();
+        P2letters ();
+        P6letters ();
+        P1letters ();
+        P1letters ();
+        P1letters ();
     }
-    public static void m2apart ()
+    public static void M2apart ()
     {
         System.out.println("M     M");   
     }
-    public static void m2close ()
+    public static void M2close ()
     {
         System.out.println("MM   MM");
     }
-    public static void m3letters ()
+    public static void M3letters ()
     {
         System.out.println("M M M M");
     }
-    public static void m4letters ()
+    public static void M4letters ()
     {
         System.out.println("M  M  M");
     }
-    public static void i4letters ()
+    public static void I4letters ()
     {
         System.out.println("IIIII");
     }
-    public static void i1letters ()
+    public static void I1letters ()
     {
         System.out.println("  I  ");
     }
-    public static void s4letters ()
+    public static void S4letters ()
     {
         System.out.println(" SSSSS ");
     }
-    public static void s2letters ()
+    public static void S2letters ()
     {
         System.out.println("S     S");
     }
-    public static void s1letters ()
+    public static void S1lletters ()
     {
         System.out.println("S");
     }
-    public static void p6letters ()
+    public static void P6letters ()
     {
         System.out.println("PPPPPP");
     }
-    public static void p2letters ()
+    public static void P2letters ()
     {
         System.out.println("P     P");
     }
-    public static void p1letters ()
+    public static void P1letters ()
     {
         System.out.println("P");
     }
-    public static void s1bletters ()
+    public static void S1rletters ()
     {
         System.out.println("      S");
     }
